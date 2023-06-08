892c0a3e57c7a7a4ded3e02a204242c330172cc4
severo <oliveira.severo@gmail.com>
2023-06-08 09:33:42 -0300

    fix: :bug: corrige erro em useEffect

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import ReactPlayer from "react-player";

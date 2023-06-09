05931d4674bc27d3230790006bf9d3baa805c8e9
severo <oliveira.severo@gmail.com>
2023-06-09 18:24:46 -0300

    feat: :rocket: Carrega exemplos do objeto

--- src/Pages/MakeGrid.jsx
+++ src/Pages/MakeGrid.jsx
@@ -4,6 +4,8 @@ import Navbar from "../components/Navbar/Navbar"
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
+import DefiningTheGrid from '../data/DefiningTheGrid'
+
 
 export default function MakeGrid() {
 
@@ -30,40 +32,37 @@ export default function MakeGrid() {
       <Navbar/>
       <h3>DEFININDO O GRID</h3>
       <div className="container_examples">
-        <div className="example">
-          <h4 className="example_title">Criando o Conteiner</h4>
-
-          <div className="example_description">
-            <p>A primeria coisa a fazer é criar um <strong>grid container</strong> com as declarações CSS <strong class="codigo">display: grid</strong> ou <strong class="codigo">display: inline-grid</strong>. Assim declarado, todos os elementos filhos diretos daquele container se transformam em <strong>grid items</strong>.</p>
-            <p>É preciso então estabelecer explicitamente o grid. No <strong>Exemplo 01</strong> temos um grid de 3 colunas com o valor <strong>1fr</strong> e duas linhas como o valor de <strong>200px</strong>.</p>
-            <p class="obs"> A unidade <strong>fr</strong> representa uma fração do espaço disponível no container do grid.</p>
-            <p>Com <strong class="codigo">grid-gap: 20px</strong> é definida a separaração tanto em colunas quanto em linhas dos <strong>grid-itens</strong> no valor de 10px.</p>
-          </div>
-
-          <div className="example_code">
-            <h5>Exemplo 01</h5>
-            <div className="header_highlighter">
-              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <button onClick={handleCopy}>Copy To Clipboard</button>
-                {copySuccess && <span>Copied to clipboard!</span>}
+        {Object.keys(DefiningTheGrid).map((exampleKey) => {
+          const example = DefiningTheGrid[exampleKey];
+          return (
+            <div className="example" key={exampleKey}>
+              <h4 className="example_title">{example.Title}</h4>
+              <div className="example_description" dangerouslySetInnerHTML={{ __html: example.Description }}></div>
+              <div className="example_code">
+                <h5>Exemplo {exampleKey}</h5>
+                <div className="header_highlighter">
+                  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <button onClick={handleCopy}>Copy To Clipboard</button>
+                    {copySuccess && <span>Copied to clipboard!</span>}
+                  </div>
+                </div>
+                <SyntaxHighlighter language="css" style={coy}>
+                  {example.Code}
+                </SyntaxHighlighter>
+              </div>
+              <div className="resultado">
+                <h5>Resultado</h5>
+                <div className={`exemplo_${exampleKey}`}>
+                  <div class="item item-01"><p>01</p></div>
+                  <div class="item item-02"><p>02</p></div>
+                  <div class="item item-03"><p>03</p></div>
+                  <div class="item item-04"><p>04</p></div>
+                  <div class="item item-05"><p>05</p></div>
+                </div>
               </div>
             </div>
-            <SyntaxHighlighter language="css" style={coy}>
-              {code}
-            </SyntaxHighlighter>
-          </div>
-
-          <div class="resultado">
-            <h5>Resultado</h5>
-            <div class="exemplo_01">
-              <div class="item item-01"><p>01</p></div>
-              <div class="item item-02"><p>02</p></div>
-              <div class="item item-03"><p>03</p></div>
-              <div class="item item-04"><p>04</p></div>
-              <div class="item item-05"><p>05</p></div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   )
--- src/data/DefiningTheGrid.js
+++ src/data/DefiningTheGrid.js
@@ -1,15 +1,18 @@
 const DefiningTheGrid = {
-  example1: {
+  1: {
     Title: "Criando o Contêiner",
     Description: "<p>A primeira coisa a fazer é criar um <strong>grid container</strong> com as declarações CSS <strong class='codigo'>display: grid</strong> ou <strong class='codigo'>display: inline-grid</strong>. Assim declarado, todos os elementos filhos diretos daquele container se transformam em <strong>grid items</strong>.</p><p>É preciso então estabelecer explicitamente o grid. No <strong>Exemplo 01</strong>, temos um grid de 3 colunas com o valor <strong>1fr</strong> e duas linhas com o valor de <strong>200px</strong>.</p><p class='obs'>A unidade <strong>fr</strong> representa uma fração do espaço disponível no container do grid.</p><p>Com <strong class='codigo'>grid-gap: 20px</strong>, é definida a separação tanto em colunas quanto em linhas dos <strong>grid items</strong> no valor de 10px.</p>",
     Code: `.cards { 
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: 200px 200px;
-    grid-gap: 20px;
-  }`
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-template-rows: 200px 200px;
+      grid-gap: 20px;
+    }`,
+    Result:{
+      NumberOfItems: 5,
+    }
   },
-  example2: {
+  2: {
     Title: "Proporção entre colunas",
     Description: "<p>A proporção entre as colunas pode ser variável. No <strong>Exemplo 2</strong> foram ciadas três colunas mas o espaço do grid foi dividido em 4 frações. As duas primeiras colunas ocupam respectivamente <strong>uma fração</strong> e a terceira coluna <strong>duas frações</strong>.</p>",
     Code: `.cards { 
@@ -17,7 +20,10 @@ const DefiningTheGrid = {
       grid-template-columns: 1fr 1fr 2fr;
       grid-template-rows: 100px 100px;
       grid-gap: 10px;
-    }`
+    }`,
+    Result:{
+      NumberOfItems: 6,
+    }
   }
 };
 

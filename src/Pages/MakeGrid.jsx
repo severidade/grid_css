b35963aa63efffb01578e7eaccd4a1c6f35743c3
severo <oliveira.severo@gmail.com>
2023-06-13 14:24:21 -0300

    feat: :rocket: cria um botao para scroll to top

--- src/Pages/MakeGrid.jsx
+++ src/Pages/MakeGrid.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import GridExamples from "../components/GridExamples/GridExamples";
 
 import DefiningTheGrid from '../data/DefiningTheGrid';
 
 export default function MakeGrid() {
+  const [buttonToTop, setButtonToTop] = useState("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= window.innerHeight * 0.1) {
+        setButtonToTop("show");
+      } else {
+        setButtonToTop("");
+      }
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="container">
-        <h3>DEFININDO O GRID</h3>
+        <h3>Definindo o grid</h3>
         <GridExamples gridData={DefiningTheGrid} />
       </div>
+      <button
+        className={`toTop ${buttonToTop}`}
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+      >
+        Topo
+      </button>
     </>
   );
 }
